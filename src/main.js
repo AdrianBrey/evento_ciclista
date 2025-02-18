e4b90d7ba199125917b0138f8caf4b7a7bd13a4b
AdrianBrey <adrianbreyb@gmail.com>
2025-02-18 12:18:43 +0100

    archivos despliegue modificados

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AppRouter from "./routes/appRouter";
 import "./styles/App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AppRouter />
-    </BrowserRouter>
+    </HashRouter> 
   </React.StrictMode>
 );
